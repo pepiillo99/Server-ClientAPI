7ffd8d6e568e3d91901460c4598316d5ca46992d
pepiillo99 <pepiillo99@hotmail.com>
2025-06-05 16:37:02 +0200

    Now can change ExpectedPacketClass on AwaitAnswerCallback

--- src/me/pepe/ServerClientAPI/Utils/AwaitAnswerCallback.java
+++ src/me/pepe/ServerClientAPI/Utils/AwaitAnswerCallback.java
@@ -7,21 +7,21 @@ public abstract class AwaitAnswerCallback {
 	private int id;
 	private long timeout = 5000;
 	private long answerTime = 0;
-	private final Class<Packet> expectedClass;
+	private Class<Packet> expectedPacketClass;
 	public AwaitAnswerCallback(ClientConnection clientConnection) {
 		this(clientConnection, 5000, null);
 	}
 	public AwaitAnswerCallback(ClientConnection clientConnection, long timeout) {
 		this(clientConnection, timeout, null);
 	}
-	public AwaitAnswerCallback(ClientConnection clientConnection, Class<Packet> expectedPacket) {
-		this(clientConnection, 5000, expectedPacket);
+	public AwaitAnswerCallback(ClientConnection clientConnection, Class<Packet> expectedPacketClass) {
+		this(clientConnection, 5000, expectedPacketClass);
 	}
-	public AwaitAnswerCallback(ClientConnection clientConnection, long timeout, Class<Packet> expectedPacket) {
+	public AwaitAnswerCallback(ClientConnection clientConnection, long timeout, Class<Packet> expectedPacketClass) {
 		this.id = clientConnection.getNextAwaitAnswerID();
 		this.timeout = timeout;
 		this.answerTime = System.currentTimeMillis();
-		this.expectedClass = expectedPacket;
+		this.expectedPacketClass = expectedPacketClass;
 	}
 	public int getID() {
 		return id;
@@ -32,8 +32,11 @@ public abstract class AwaitAnswerCallback {
 	public boolean isTimeOut() {
 		return (answerTime + 5000) - System.currentTimeMillis() <= 0;
 	}
+	public void setExpectedPacket(Class<Packet> expectedPacketClass) {
+		this.expectedPacketClass = expectedPacketClass;
+	}
 	public boolean isCorrectPacket(Packet packet) {
-		return expectedClass == null || expectedClass.isInstance(packet);
+		return expectedPacketClass == null || expectedPacketClass.isInstance(packet);
 	}
 	public abstract void onAnswer(Packet packet);
 	public abstract void onTimeOut();
